0104fd219f2c6db84bec4ecee9cb6671f3de0fa2
Raji Mustapha <rajimustapha30@gmail.com>
2021-01-27 08:54:36 +0100

    fix

--- assets/js/kuizu.js
+++ assets/js/kuizu.js
@@ -18,13 +18,4 @@ links.forEach(link=>{
     });
   })
 }) 
-//Slider
- var swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-      },
-    });
+
